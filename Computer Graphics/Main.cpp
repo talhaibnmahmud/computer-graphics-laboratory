5eb4f6fe5e3569575a52b5e3f9999d47fdcdb263
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2022-08-30 00:34:18 +0600

    🚀 Get started in OpenGL with GLFW

--- Computer Graphics/Main.cpp
+++ Computer Graphics/Main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+		
+		// Draw a triangle
+        glBegin(GL_TRIANGLES);
+		glColor3f(1.0, 1.0, 1.0);
+		glVertex2f(-0.5, -0.5);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex2f(0.5, -0.5);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex2f(0.0, 0.5);
+		glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 	
 	std::cin.get();
 }
