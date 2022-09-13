be9581a46d41bb59c92074854eca06a524a2eb90
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2022-09-13 09:20:10 +0600

    ✅Assignment: Lab-1

---
+++ Lab-1/Assignment.cpp
@@ -0,0 +1,447 @@
+#include <GL/glut.h>
+
+#include <cmath>
+
+#include "Assignment.h"
+
+
+static constexpr auto W_WIDTH = 852;
+static constexpr auto W_HEIGHT = 480;
+
+static GLfloat Txval = 0.0f, Tyval = 0.0f, Tzval = 0.0f;
+static GLfloat alpha = 0.0f, theta = 0.0f;
+static GLfloat axis_x = 0.0f, axis_y = 0.0f, axis_z = 0.0f;
+static bool bRotate = false, uRotate = false;
+
+static GLfloat eyeX = 2, eyeY = 3, eyeZ = 10;
+static GLfloat lookX = 2, lookY = 0, lookZ = 0;
+
+static constexpr GLfloat cube[8][3] =
+{
+	{0.0, 1.0, 1.0},
+	{0.0, 1.0, 0.0},
+	{1.0, 1.0, 1.0},
+	{1.0, 1.0, 0.0},
+	{1.0, 0.0, 1.0},
+	{1.0, 0.0, 0.0},
+	{0.0, 0.0, 1.0},
+	{0.0, 0.0, 0.0}
+};
+
+static constexpr GLuint indices[2][8] =
+{
+	{0,1,2,3,4,5,6,7},
+	{3,5,1,7,0,6,2,4}
+};
+
+
+static void draw_cube(GLfloat r, GLfloat g, GLfloat b)
+{
+	glColor3f(r, g, b);
+	glBegin(GL_QUAD_STRIP);
+	for (GLint i = 0; i < 2; i++)
+	{
+
+		// glColor3f(colors[i][0],colors[i][1],colors[i][2]);
+		glVertex3fv(&cube[indices[i][0]][0]);
+		glVertex3fv(&cube[indices[i][1]][0]);
+		glVertex3fv(&cube[indices[i][2]][0]);
+		glVertex3fv(&cube[indices[i][3]][0]);
+		glVertex3fv(&cube[indices[i][4]][0]);
+		glVertex3fv(&cube[indices[i][5]][0]);
+		glVertex3fv(&cube[indices[i][6]][0]);
+		glVertex3fv(&cube[indices[i][7]][0]);
+
+	}
+
+	glEnd();
+}
+
+void draw_wall()
+{
+	glPushMatrix();
+	glTranslatef(-5.05f, -6.0f, -2.0f);
+	glScalef(0.1f, 6.0f, 7.0f);
+	draw_cube(0.0f, 0.0f, 1.0f);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-5.0f, -6.0f, -2.0f);
+	glScalef(10.0f, 6.0f, 0.01f);
+	draw_cube(0.0f, 0.0f, 1.0f);
+	glPopMatrix();
+}
+
+void draw_door()
+{
+	glPushMatrix();
+	glTranslatef(-4.5f, -6.0f, -1.9f);
+	glScalef(3.5f, 5.0f, .01f);
+	draw_cube(1.0f, 0.0f, 0.0f);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-4.35f, -5.75f, -1.85f);
+	glScalef(1.5f, 4.5f, .01f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-2.6f, -5.75f, -1.85f);
+	glScalef(1.5f, 4.5f, 0.01f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-2.25f, -3.25f, -1.8f);
+	glScalef(.05f, 0.75f, 0.1f);
+	draw_cube(1.0f, 1.0f, 1.0f);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-3.25f, -3.25f, -1.8f);
+	glScalef(.05f, 0.75f, 0.1f);
+	draw_cube(1.0f, 1.0f, 1.0f);
+	glPopMatrix();
+}
+
+void draw_floor()
+{
+	glPushMatrix();
+	glTranslatef(-5.0f, -6.0f, -2.0f);
+	glScalef(10.0f, 0.01f, 7.0f);
+	draw_cube(0.3f, 0.3f, 0.3f);
+	glPopMatrix();
+}
+
+void draw_bed()
+{
+	// top left leg
+	glPushMatrix();
+	glTranslatef(-5.0f, -6.0f, 3.25f);
+	glScalef(0.5f, 3.0f, 0.25f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// top right leg
+	glPushMatrix();
+	glTranslatef(-5.0f, -6.0f, 0.25f);
+	glScalef(0.5f, 3.0f, 0.25f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom right leg
+	glPushMatrix();
+	glTranslatef(0.25f, -6.0f, 0.25f);
+	glScalef(0.5f, 3.0f, 0.25f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom left leg
+	glPushMatrix();
+	glTranslatef(0.25f, -6.0f, 3.25f);
+	glScalef(0.5f, 3.0f, 0.25f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// head plate
+	glPushMatrix();
+	glTranslatef(-5.0f, -5.25f, 0.25f);
+	glScalef(0.5f, 1.75f, 3.0f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// feet plate
+	glPushMatrix();
+	glTranslatef(0.25f, -5.25f, 0.25f);
+	glScalef(0.5f, 1.75f, 3.0f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bed
+	glPushMatrix();
+	glTranslatef(-5.0f, -5.25f, 0.25f);
+	glScalef(5.5f, 1.0f, 3.0f);
+	draw_cube(0.6f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// matress
+	glPushMatrix();
+	glTranslatef(-5.0f, -4.25f, 0.25f);
+	glScalef(5.5f, 0.5f, 3.0f);
+	draw_cube(1.0f, 1.0f, 1.0f);
+	glPopMatrix();
+
+	// blanket
+	glPushMatrix();
+	glTranslatef(-3.0f, -4.20f, 0.26f);
+	glScalef(3.5f, 0.51f, 3.01f);
+	draw_cube(0.1f, 1.0f, 0.1f);
+	glPopMatrix();
+
+	// pillow
+	glPushMatrix();
+	glTranslatef(-4.5f, -3.75f, 0.75f);
+	glScalef(1.25f, 0.25f, 2.0f);
+	draw_cube(0.6f, 0.6f, 0.6f);
+	glPopMatrix();
+}
+
+void draw_table()
+{
+	// top left leg
+	glPushMatrix();
+	glTranslatef(3.5f, -6.0f, 3.25f);
+	glScalef(0.15f, 2.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// top right leg
+	glPushMatrix();
+	glTranslatef(3.5f, -6.0, 0.75f);
+	glScalef(0.15f, 2.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom left leg
+	glPushMatrix();
+	glTranslatef(4.75f, -6.0f, 3.25f);
+	glScalef(0.15f, 2.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom right leg
+	glPushMatrix();
+	glTranslatef(4.75f, -6.0f, 0.75f);
+	glScalef(0.15f, 2.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// table top
+	glPushMatrix();
+	glTranslatef(3.375f, -3.65f, 0.65f);
+	glScalef(1.75f, 0.15f, 3.0f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+}
+
+void draw_chair()
+{
+	// top left leg
+	glPushMatrix();
+	glTranslatef(2.25f, -6.0f, 3.0f);
+	glScalef(0.15f, 3.0f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// top right leg
+	glPushMatrix();
+	glTranslatef(2.25f, -6.0f, 1.75f);
+	glScalef(0.15f, 3.0f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom left leg
+	glPushMatrix();
+	glTranslatef(3.75f, -6.0f, 3.0f);
+	glScalef(0.15f, 1.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// bottom right leg
+	glPushMatrix();
+	glTranslatef(3.75f, -6.0f, 1.75f);
+	glScalef(0.15f, 1.5f, 0.15f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// chair base
+	glPushMatrix();
+	glTranslatef(2.25f, -4.5f, 1.7f);
+	glScalef(1.75f, 0.1f, 1.5f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+
+	// chair back
+	glPushMatrix();
+	glTranslatef(2.25f, -3.75f, 1.7f);
+	glScalef(0.1f, 0.70f, 1.5f);
+	draw_cube(0.2f, 0.1f, 0.1f);
+	glPopMatrix();
+}
+
+void draw_laptop()
+{
+	// laptop base
+	glPushMatrix();
+	glTranslatef(3.75f, -3.5f, 1.7f);
+	glScalef(1.0f, 0.01f, 1.0f);
+	draw_cube(0.0f, 0.0f, 0.0f);
+	glPopMatrix();
+
+	// laptop front
+	glPushMatrix();
+	glTranslatef(4.75f, -3.5f, 1.7f);
+	glScalef(0.01f, 1.0f, 1.0f);
+	draw_cube(0.0f, 0.0f, 0.0f);
+	glPopMatrix();
+	// laptop display
+	glPushMatrix();
+	glTranslatef(4.74f, -3.45f, 1.75f);
+	glScalef(0.01f, 0.9f, 0.9f);
+	draw_cube(1.0f, 1.0f, 1.0f);
+	glPopMatrix();
+}
+
+void draw_scene()
+{
+	draw_wall();
+	draw_door();
+	draw_floor();
+	draw_bed();
+	draw_table();
+	draw_chair();
+	draw_laptop();
+}
+
+void display_func()
+{
+	// Set the background color to mid gray
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+	// Clear the color and depth buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Set the matrix mode to Projection
+	glMatrixMode(GL_PROJECTION);
+	// Load the identity matrix
+	glLoadIdentity();
+	// Load the frustum
+	glFrustum(-5, 5, -5, 5, 5, 200);
+
+	// Set the matrix mode to Model View
+	glMatrixMode(GL_MODELVIEW);
+	// Load the identity matrix
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(eyeX, eyeY, eyeZ, lookX, lookY, lookZ, 0, 1, 0);
+
+	// Set the rotation
+	glRotatef(alpha, axis_x, axis_y, axis_z);
+	glRotatef(theta, axis_x, axis_y, axis_z);
+
+	// Set the viewport 
+	glViewport(0, 0, W_WIDTH, W_HEIGHT);
+
+	// draw the scene
+	draw_scene();
+
+	// Flush the buffer
+	glFlush();
+	// Swap buffers
+	glutSwapBuffers();
+}
+
+void keyboard_func(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'R':	// rotate about y-axis
+		bRotate = !bRotate;
+		axis_y = -1.0;
+		break;
+	case 'r':
+		bRotate = !bRotate;
+		axis_y = 1.0;
+		break;
+	case 'T':	// rotate about x-axis
+		bRotate = !bRotate;
+		axis_x = -1.0;
+		break;
+	case 't':
+		bRotate = !bRotate;
+		axis_x = 1.0;
+		break;
+	case 'Y':	// rotate about z-axis
+		bRotate = !bRotate;
+		axis_z = -1.0;
+		break;
+	case 'y':
+		bRotate = !bRotate;
+		axis_z = 1.0;
+		break;
+
+	case '+':   //zoom in
+		eyeZ++;
+		lookZ++;
+		break;
+	case '-':   //zoom out
+		eyeZ--;
+		lookZ--;
+		break;
+	case ']':   // slide up
+		eyeY++;
+		lookY++;
+		break;
+	case '[':   // slide down
+		eyeY--;
+		lookY--;
+		break;
+	case 'x':   // slide right
+		eyeX++;
+		lookX++;
+		break;
+	case 'z':   // slide left
+		eyeX--;
+		lookX--;
+		break;
+
+		// case 'a' for Roll left
+	case 'a':
+		lookX--;
+		break;
+		// case 'd' for Roll right
+	case 'd':
+		lookX++;
+		break;
+		// case 'w' for Pitch up
+	case 'w':
+		lookY++;
+		break;
+		// case 's' for Pitch down
+	case 's':
+		lookY--;
+		break;
+		// case 'q' for Yaw left
+	case 'q':
+		lookZ--;
+		break;
+		// case 'e' for Yaw right
+	case 'e':
+		lookZ++;
+		break;
+
+	case 27:	// Escape key
+		exit(EXIT_SUCCESS);
+
+	default:
+		break;
+	}
+}
+
+void idle_func()
+{
+	if (bRotate == true)
+	{
+		theta += 0.05f;
+		if (theta > 360.0f)
+			theta -= 360.0f * floor(theta / 360.0f);
+	}
+	if (uRotate == true)
+	{
+		alpha += 0.05f;
+		if (alpha > 360.0f)
+			alpha -= 360.0f * floor(alpha / 360.0f);
+	}
+
+	glutPostRedisplay();
+}
--- Lab-1/Main.cpp
+++ Lab-1/Main.cpp
@@ -2,9 +2,101 @@
 
 #include <iostream>
 
+#include "Assignment.h"
+
 
 constexpr auto WINDOW_WIDTH = 852;
 constexpr auto WINDOW_HEIGHT = 480;
+
+
+static GLfloat p_vertices[5][3] = {
+	{0.0f, 0.0f, 0.0f},				//point index 0
+	{0.0f, 0.0f, 2.0f},				//point index 1
+	{2.0f, 0.0f, 2.0f},				//point index 2					{2.0f, 0.0f, 0.0f},  //point index 3
+	{1.0f, 4.0f, 1.0f}				//point index 4
+};
+
+static GLubyte p_indices[4][3] = {
+	{4, 1, 2},						// indices for drawing the triangle plane 1
+	{4, 2, 3},						// indices for drawing the triangle plane 2
+	{4, 3, 0},						// indices for drawing the triangle plane 3
+	{4, 0, 1}						// indices for drawing the triangle plane 4
+};
+
+static GLubyte quad_indices[1][4] = {
+	{0, 3, 2, 1}					// indeces for drawing the quad plane
+};
+
+static GLfloat color_values[5][3] = {
+	{0.0f, 0.0f, 1.0f},				//color for point index 0
+	{0.5f, 0.0f, 1.0f},				//color for point index 1
+	{0.0f, 1.0f, 0.0f},				//color for point index 2
+	{0.0f, 1.0f, 1.0f},				//color for point index 3
+	{0.8f, 0.0f, 0.0f}				//color for point index 4
+};
+
+static void getNormal3p
+(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2, GLfloat x3, GLfloat y3, GLfloat z3) {
+	GLfloat Ux, Uy, Uz, Vx, Vy, Vz, Nx, Ny, Nz;
+
+	Ux = x2 - x1;
+	Uy = y2 - y1;
+	Uz = z2 - z1;
+
+	Vx = x3 - x1;
+	Vy = y3 - y1;
+	Vz = z3 - z1;
+
+	Nx = Uy * Vz - Uz * Vy;
+	Ny = Uz * Vx - Ux * Vz;
+	Nz = Ux * Vy - Uy * Vx;
+
+	glNormal3f(Nx, Ny, Nz);
+}
+
+void pyramid()
+{
+	glBegin(GL_TRIANGLES);
+	for (GLint i = 0; i < 4; i++) {
+		getNormal3p(
+			p_vertices[p_indices[i][0]][0], p_vertices[p_indices[i][0]][1], p_vertices[p_indices[i][0]][2],
+			p_vertices[p_indices[i][1]][0], p_vertices[p_indices[i][1]][1], p_vertices[p_indices[i][1]][2],
+			p_vertices[p_indices[i][2]][0], p_vertices[p_indices[i][2]][1], p_vertices[p_indices[i][2]][2]
+		);
+
+		glVertex3fv(&p_vertices[p_indices[i][0]][0]);
+		glVertex3fv(&p_vertices[p_indices[i][1]][0]);
+		glVertex3fv(&p_vertices[p_indices[i][2]][0]);
+	}
+	glEnd();
+
+	glBegin(GL_QUADS);
+	for (GLint i = 0; i < 1; i++) {
+		getNormal3p(
+			p_vertices[quad_indices[i][0]][0], p_vertices[quad_indices[i][0]][1], p_vertices[quad_indices[i][0]][2],
+			p_vertices[quad_indices[i][1]][0], p_vertices[quad_indices[i][1]][1], p_vertices[quad_indices[i][1]][2],
+			p_vertices[quad_indices[i][2]][0], p_vertices[quad_indices[i][2]][1], p_vertices[quad_indices[i][2]][2]
+		);
+
+		glVertex3fv(&p_vertices[quad_indices[i][0]][0]);
+		glVertex3fv(&p_vertices[quad_indices[i][1]][0]);
+		glVertex3fv(&p_vertices[quad_indices[i][2]][0]);
+		glVertex3fv(&p_vertices[quad_indices[i][3]][0]);
+	}
+	glEnd();
+}
+
+void ownTranslatef(GLfloat dx, GLfloat dy, GLfloat dz) {
+	GLfloat m[16];
+
+	m[0] = 1;	m[4] = 0;  	 m[8] = 0;   	m[12] = dx;
+	m[1] = 0; 	m[5] = 1; 	 m[9] = 0;   	m[13] = dy;
+	m[2] = 0; 	m[6] = 0;	 m[10] = 1; 	m[14] = dz;
+	m[3] = 0; 	m[7] = 0; 	 m[11] = 0; 	m[15] = 1;
+
+	glMatrixMode(GL_MODELVIEW);
+	glMultMatrixf(m);
+}
 
 
 static void triangle()
@@ -45,10 +137,10 @@ static void display()
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Lab-1");
+	glutCreateWindow("Lab-1: Assignment");
 	
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
@@ -57,7 +149,9 @@ int main(int argc, char** argv)
 	std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << std::endl;
 	std::cout << "OpenGL Version:  " << glGetString(GL_VERSION) << std::endl;
 
-	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard_func);
+	glutDisplayFunc(display_func);
+	glutIdleFunc(idle_func);
 	
 	glutMainLoop();
 	
