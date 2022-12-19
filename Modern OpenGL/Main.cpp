35c4644f30853e2b1a5d8291d6487ab5723576ee
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2022-12-19 07:45:45 +0600

    💡Pyramid drawn

--- Modern OpenGL/Main.cpp
+++ Modern OpenGL/Main.cpp
@@ -16,6 +16,11 @@
 #include "ElementBufferObject.h"
 
 
+constexpr auto width = 800;
+constexpr auto height = 800;
+
+
+
 int main(int argc, char* argv[])
 {
 	glfwInit();
@@ -25,18 +30,23 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] = {
-		-0.5f,	-0.5f,	0.0f,		1.0f,	0.0f,	0.0f,		0.0f, 0.0f,		// lower left
-		-0.5f,	 0.5f,	0.0f,		0.0f,	1.0f,	0.0f,		0.0f, 1.0f,		// upper left
-		 0.5f,	 0.5f,	0.0f,		0.0f,	0.0f,	1.0f,		1.0f, 1.0f,		// top right
-		 0.5f,	-0.5f,	0.0f,		1.0f,	1.0f,	1.0f,		1.0f, 0.0f,		// lower right
+		-0.5f,	 0.0f,	 0.5f,		0.85f,	0.70f,	0.45f,		0.0f, 0.0f,		// lower left
+		-0.5f,	 0.0f,	-0.5f,		0.85f,	0.70f,	0.45f,		5.0f, 0.0f,		// upper left
+		 0.5f,	 0.0f,	-0.5f,		0.85f,	0.70f,	0.45f,		0.0f, 0.0f,		// top right
+		 0.5f,	 0.0f,	 0.5f,		0.85f,	0.70f,	0.45f,		5.0f, 0.0f,		// lower right
+		 0.0f,	 0.8f,	 0.0f,		0.95f,	0.85f,	0.75f,		2.5f, 5.0f,		// lower right
 	};
 
 	GLuint indices[] = {
-		0, 2, 1,		// first triangle
-		0, 3, 2,		// second triangle
+		0, 1, 2,
+		0, 2, 3,
+		0, 1, 4,
+		1, 2, 4,
+		2, 3, 4,
+		3, 0, 4,
 	};
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Modern OpenGL", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Modern OpenGL", nullptr, nullptr);
 
 	if (window == nullptr)
 	{
@@ -59,7 +69,7 @@ int main(int argc, char* argv[])
 	std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
 	std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, width, height);
 
 	Shader shader("default.vert", "default.frag");
 	
@@ -84,22 +94,49 @@ int main(int argc, char* argv[])
 	texture.texUnit(shader, "texture0", 0);
 
 	glClearColor(0.075f, 0.15f, 0.15f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glfwSwapBuffers(window);
+
+	float rotation = 0.0f;
+	double previousTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.075f, 0.15f, 0.15f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader.Activate();
+
+		double currentTime = glfwGetTime();
+		if (currentTime - previousTime >= 1 / 60)
+		{
+			rotation += 0.05f;
+			previousTime = currentTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+		
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int uniModel = glGetUniformLocation(shader.ID, "model");
+		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+		int uniView = glGetUniformLocation(shader.ID, "view");
+		glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+		int uniProj = glGetUniformLocation(shader.ID, "projection");
+		glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 		
 		glUniform1f(uniScale, 0.5f);
 		texture.Bind();
 		
 		vao.Bind();
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
--- Modern OpenGL/default.vert
+++ Modern OpenGL/default.vert
@@ -15,9 +15,13 @@ out vec2 outTexCoord;
 // Scale Control
 uniform float scale;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 	outColor = aColor;
 	outTexCoord = aTexCoord;
 }
